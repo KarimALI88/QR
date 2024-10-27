438535677f93ad5214694479404424b887c4bd45
karim ali <karimalielsayed88@gmail.com>
2024-10-27 18:25:26 +0300

    change route

--- src/pages/UserLayout.jsx
+++ src/pages/UserLayout.jsx
@@ -12,8 +12,8 @@ const UserLayout = () => {
         <Route path='' element={<Home />}/>
         <Route path='/login' element={<Login />}/>
         <Route path='/register' element={<Register />}/>
-        <Route path='/generate-qr' element={<QrForm />}/>
-        <Route path='/qr' element={<PackageOneTwo />}/>
+        <Route path='/qr' element={<QrForm />}/>
+        <Route path='/generate-qr' element={<PackageOneTwo />}/>
     </Routes>
   )
 }
